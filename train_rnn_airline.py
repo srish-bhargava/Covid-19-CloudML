489b417986538f57506c1c10232fb34e7eb6fbdf
Srishti Bhargava <srishti.bhargava1995@gmail.com>
2020-04-29 20:02:39 -0400

    Add files via upload

---
+++ train_rnn_airline.py
@@ -0,0 +1,125 @@
+import numpy as np
+import pandas as pd
+#import h5py
+#from collections import defaultdict
+import re
+#from bs4 import BeautifulSoup
+import sys
+import os
+import csv
+import keras
+os.environ['KERAS_BACKEND']='theano'
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils.np_utils import to_categorical
+from keras.layers import Embedding
+from keras.layers import Dense, Input, Flatten
+from keras.layers import Conv1D, MaxPooling1D, Embedding, Dropout, LSTM, GRU, Bidirectional
+from keras.models import Model
+from keras.callbacks import ModelCheckpoint
+#import matplotlib.pyplot as plt
+#plt.switch_backend('agg')
+from keras import backend as K
+from keras.engine.topology import Layer, InputSpec
+from keras import initializers
+
+MAX_SEQUENCE_LENGTH = 1000
+MAX_NB_WORDS = 20000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.1
+
+tweets = pd.read_csv("Airlinetweets.csv", encoding= 'unicode_escape')
+#f = h5py.File("model_rnn.hdf5", "w")
+macronum=sorted(set(tweets['type']))
+macro_to_id = dict((note, number) for number, note in enumerate(macronum))
+
+def fun(i):
+    return macro_to_id[i]
+
+tweets['type']=tweets['type'].apply(fun)
+
+labels = []
+for idx in tweets['type']:
+    labels.append(idx)
+
+texts = []
+for tweet in tweets['text']:
+    tweet = tweet.lower() # convert text to lower-case
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet) # remove URLs
+    tweet = re.sub('@[^\s]+', 'AT_USER', tweet) # remove usernames
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet) 
+    #print (tweet)
+    texts.append(tweet)
+
+tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+
+word_index = tokenizer.word_index
+
+print('Number of Unique Tokens',len(word_index))
+
+data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+
+labels = to_categorical(np.asarray(labels))
+print('Shape of Data Tensor:', data.shape)
+print('Shape of Label Tensor:', labels.shape)
+
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
+
+x_train = data[:-nb_validation_samples]
+y_train = labels[:-nb_validation_samples]
+x_val = data[-nb_validation_samples:]
+y_val = labels[-nb_validation_samples:]
+
+embeddings_index = {}
+f = open('glove.6B.100d.txt',encoding='utf8')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Total %s word vectors in Glove 6B 100d.' % len(embeddings_index))
+
+embedding_matrix = np.random.random((len(word_index) + 1, EMBEDDING_DIM))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
+
+embedding_layer = Embedding(len(word_index) + 1,
+                            EMBEDDING_DIM,
+                            weights=[embedding_matrix],
+                            input_length=MAX_SEQUENCE_LENGTH,
+                            trainable=True)
+
+sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+embedded_sequences = embedding_layer(sequence_input)
+l_lstm = Bidirectional(LSTM(100))(embedded_sequences)
+preds = Dense(len(macronum), activation='softmax')(l_lstm)
+model = Model(sequence_input, preds)
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['acc'])
+
+print("Bidirectional LSTM")
+print (model.summary())
+
+filepath = '/model_rnn.hdf5'
+#model.fit(x_train, y_train, validation_data=(x_val, y_val),epochs=1, batch_size=2)
+cp= ModelCheckpoint('rnn_model_8.hdf5',monitor='val_acc',verbose=1,save_best_only=True)
+history=model.fit(x_train, y_train, validation_data=(x_val, y_val),epochs=3, batch_size=2,callbacks=[cp])
+#print (his)
+#model.save(/model_rnn.hdf5)
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+
+model.save_weights("model.h5")
