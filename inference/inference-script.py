900c4f9b8910f98eb78f55bf8aafff7c691561fa
Srishti Bhargava <srishti.bhargava1995@gmail.com>
2020-05-03 01:27:00 -0400

    inference updated with flask

--- inference/inference-script.py
+++ inference/inference-script.py
@@ -1,10 +1,15 @@
+import os
+import io
+import flask
 import json
 import keras
 import keras.preprocessing.text as kpt
 from keras.models import model_from_json
 import numpy as np
 from keras.preprocessing.text import Tokenizer
+from flask import Flask, jsonify, request
 
+app = flask.Flask(__name__)
 # we're still going to use a Tokenizer here, but we don't need to fit it
 # tokenizer = Tokenizer(num_words=3000)
 tokenizer = Tokenizer(num_words=3000)
@@ -56,7 +61,7 @@ while 1:
 
     pred = model.predict(input_value)
     print("%s sentiment; %f%% confidence" % (labels[np.argmax(pred)], pred[0][np.argmax(pred)] * 100))
-'''
+
 # TO DELETE WHEN FLASK CREATED
 evalSentence = "good awesome amazing"
 print("tweet to test: " + evalSentence)
@@ -64,3 +69,26 @@ testArr = convert_text_to_index_array(evalSentence)
 input_value = tokenizer.sequences_to_matrix([testArr], mode='binary')
 pred = model.predict(input_value)
 print("%s sentiment; %f%% confidence" % (labels[np.argmax(pred)], pred[0][np.argmax(pred)] * 100))
+'''
+@app.route("/")
+def predict():
+    data = {"success": False}
+    # ensure an image was properly uploaded to our endpoint
+    #if flask.request.method == "POST":
+    tweet = request.args['name']
+    #check if tweet length is 0, break out
+    testArr = convert_text_to_index_array(tweet)
+    input_value = tokenizer.sequences_to_matrix([testArr], mode='binary')
+    pred = model.predict(input_value)
+    print("%s sentiment; %f%% confidence" % (labels[np.argmax(pred)], pred[0][np.argmax(pred)] * 100))
+    data = {}
+    data["predictions"] = str(labels[np.argmax(pred)])
+    data["confidence"] = str(pred[0][np.argmax(pred)] * 100) + " %"
+    print(data)
+    return flask.jsonify(data)
+
+
+if __name__ == '__main__':
+    print("Loading model and starting Flask Server..")
+    #load_model()
+    app.run(host='0.0.0.0', port=8001, threaded=False)
